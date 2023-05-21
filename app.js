b66f39163d8ec8974aa20d4901bb2720ede4de47
Luka Mendonça <lukamendoncadev@proton.me>
2023-05-21 12:45:24 -0300

    Fix somes code redundancies and move the inputs values to inside the functions

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const dayInput = document.getElementById('day');
-const monthInput = document.getElementById('month');
-const yearInput = document.getElementById('year');
-const arrowBtn = document.querySelector('.arrow');
-
 const dayText = document.querySelector('.day span');
 const monthText = document.querySelector('.month span');
 const yearText = document.querySelector('.year span');
+const arrowBtn = document.querySelector('.arrow');
 
 arrowBtn.addEventListener('click', calculateAge);
 
 
 function calculateAge() {
+    const dayInput = document.getElementById('day').value.trim();
+    const monthInput = document.getElementById('month').value.trim();
+    const yearInput = document.getElementById('year').value.trim();
+
+    if (!dayInput || !monthInput || !yearInput) return;
+    if (isNaN(dayInput) || +dayInput < 1 || +dayInput > 31) return;
+    if (isNaN(monthInput) || +monthInput < 1 || +monthInput > 12) return;
+    if (isNaN(yearInput) || +yearInput > new Date().getFullYear()) return;
+
     const today = moment();
-    const age = moment(`${+yearInput.value}-${+monthInput.value}-${+dayInput.value}`);
+    const age = moment(`${+yearInput}-${+monthInput}-${+dayInput}`);
     const diff = moment.preciseDiff(today, age, true);
 
     const { years, months, days } = diff;
